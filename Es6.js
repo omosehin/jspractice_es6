a9c3883b3427f572f46b1ce3a129eb4eac6476e5
omosehin <omosehin14@gmail>
2019-01-02 17:10:57 +0100

    Object_assign

--- Es6.js
+++ Es6.js
@@ -78,42 +78,7 @@ console.log(".............Const.............");
   console.log(qw); //[ 1, 2, 3, 4 ]
 }
 
-console.log("...................spread and rest...............");
-function foov(x, y, z) {
-  console.log(x, y, z);
-}
-foov(...[1, 2, 3]); // 1 2 3
 
-console.log(".................spread and rest 1...............");
-
-{
-  var ab = [2, 3, 4];
-  var b = [1, ...ab, 5];
-  console.log(b); //[ 1, 2, 3, 4, 5 ]
-}
-
-console.log("......REST 2 Gather a set of values together into an array...");
-
-function fooy(x, y, ...z) {
-  console.log(x, y, z);
-}
-fooy(1, 2, 3, 4, 5); //1 2 [ 3, 4, 5 ]
-console.log("................... rest 3...............");
-
-function foow(...args) {
-  console.log(args);
-}
-foow(1, 2, 3, 4, 5); //[ 1, 2, 3, 4, 5 ]
-
-console.log(".REST 4 ...args is called rest parameter.....");
-
-{
-  function foob(...args) {
-    args.shift();
-    console.log(args);
-  }
-  foob(4, 5, 6, 7, 8, 9); // [ 5, 6, 7, 8, 9 ]
-}
 
 console.log(".......DESTRUCTURING on function ES5/ES6.......");
 
---
+++ Let_Const.js
@@ -0,0 +1,23 @@
+// //Block-Scoped Declarations
+// var a=2;
+// (function IIFE(){
+//     var a=3;
+//     console.log(a);
+// })();
+// console.log(a);
+
+// var funcs=[];
+// for (let i=0;i<5; i++){
+//     funcs.push(function(){
+//         console.log(i)
+//     })
+// }
+
+// funcs[4]();
+
+console.log('..................................');
+
+for (var i=0; i<10;i++){
+    console.log(i)
+}
+console.log ('after loop',i)
